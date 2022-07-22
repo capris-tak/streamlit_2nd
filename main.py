3ffa87ab8666b8268af32077adc2eb7be8762696
capris-tak <109158237+capris-tak@users.noreply.github.com>
2022-07-23 02:04:30 +0900

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from PIL import Image
 
 st.title('Image Colorization')
 
-image = Image.open('imgs/c.jpeg')
+image = Image.open('imgs/1960.jpg')
 
 
 import argparse
@@ -12,7 +12,7 @@ import matplotlib.pyplot as plt
 from colorizers import *
 
 parser = argparse.ArgumentParser()
-parser.add_argument('-i','--img_path', type=str, default='imgs/c.jpeg')
+parser.add_argument('-i','--img_path', type=str, default='imgs/1960.jpg')
 parser.add_argument('--use_gpu', action='store_true', help='whether to use GPU')
 parser.add_argument('-o','--save_prefix', type=str, default='saved', help='will save into this file with {eccv16.png, siggraph17.png} suffixes')
 opt = parser.parse_args()
