f006820784e81436e0281eb12c6ae83221bffb23
capris-tak <109158237+capris-tak@users.noreply.github.com>
2022-07-23 02:44:25 +0900

    Update main.py

--- main.py
+++ main.py
@@ -5,54 +5,55 @@ st.title('Image Colorization')
 
 uploaded_image = st.file_uploader('Choose an image..',type=['png', 'jpg','jpeg'])
 
-image = Image.open(uploaded_image)
+if ploaded_image is not None:
+	#image = Image.open(uploaded_image)
 
-#with io.BytesIO() as output:
-#	img.save(output, format='JPEG')
-#	binary_img = outout.getvalue()#バイナリ取得
+	#with io.BytesIO() as output:
+	#	img.save(output, format='JPEG')
+	#	binary_img = outout.getvalue()#バイナリ取得
 
-import argparse
-import matplotlib.pyplot as plt
+	import argparse
+	import matplotlib.pyplot as plt
 
-from colorizers import *
+	from colorizers import *
 
-#parser = argparse.ArgumentParser()
-#parser.add_argument('-i','--img_path', type=str, default='imgs/ansel_adams2.jpg')
-#parser.add_argument('--use_gpu', action='store_true', help='whether to use GPU')
-#parser.add_argument('-o','--save_prefix', type=str, default='saved', help='will save into this file with {eccv16.png, siggraph17.png} suffixes')
-#opt = parser.parse_args()
+	#parser = argparse.ArgumentParser()
+	#parser.add_argument('-i','--img_path', type=str, default='imgs/ansel_adams2.jpg')
+	#parser.add_argument('--use_gpu', action='store_true', help='whether to use GPU')
+	#parser.add_argument('-o','--save_prefix', type=str, default='saved', help='will save into this file with {eccv16.png, siggraph17.png} suffixes')
+	#opt = parser.parse_args()
 
-# load colorizers
-colorizer_eccv16 = eccv16(pretrained=True).eval()
-colorizer_siggraph17 = siggraph17(pretrained=True).eval()
-if(opt.use_gpu):
-	colorizer_eccv16.cuda()
-	colorizer_siggraph17.cuda()
+	# load colorizers
+	colorizer_eccv16 = eccv16(pretrained=True).eval()
+	colorizer_siggraph17 = siggraph17(pretrained=True).eval()
+	if(opt.use_gpu):
+		colorizer_eccv16.cuda()
+		colorizer_siggraph17.cuda()
 
-# default size to process images is 256x256
-# grab L channel in both original ("orig") and resized ("rs") resolutions
-#img = load_img(opt.img_path)
-(tens_l_orig, tens_l_rs) = preprocess_img(image, HW=(256,256))
-if(opt.use_gpu):
-	tens_l_rs = tens_l_rs.cuda()
+	# default size to process images is 256x256
+	# grab L channel in both original ("orig") and resized ("rs") resolutions
+	#img = load_img(opt.img_path)
+	(tens_l_orig, tens_l_rs) = preprocess_img(image, HW=(256,256))
+	if(opt.use_gpu):
+		tens_l_rs = tens_l_rs.cuda()
 
-# colorizer outputs 256x256 ab map
-# resize and concatenate to original L channel
-img_bw = postprocess_tens(tens_l_orig, torch.cat((0*tens_l_orig,0*tens_l_orig),dim=1))
-out_img_eccv16 = postprocess_tens(tens_l_orig, colorizer_eccv16(tens_l_rs).cpu())
-out_img_siggraph17 = postprocess_tens(tens_l_orig, colorizer_siggraph17(tens_l_rs).cpu())
+	# colorizer outputs 256x256 ab map
+	# resize and concatenate to original L channel
+	img_bw = postprocess_tens(tens_l_orig, torch.cat((0*tens_l_orig,0*tens_l_orig),dim=1))
+	out_img_eccv16 = postprocess_tens(tens_l_orig, colorizer_eccv16(tens_l_rs).cpu())
+	out_img_siggraph17 = postprocess_tens(tens_l_orig, colorizer_siggraph17(tens_l_rs).cpu())
 
 
-col1, col2, col3 = st.columns(3)
-with col1:
-    st.header("Original")
-    st.image("imgs/ansel_adams2.jpg", use_column_width=True)
-with col2:
-    st.header("ECCV 16")
-    st.image(out_img_eccv16, use_column_width=True)
-with col3:
-    st.header("SIGGRAPH 17")
-    st.image(out_img_siggraph17, use_column_width=True)
+	col1, col2, col3 = st.columns(3)
+	with col1:
+	    st.header("Original")
+	    st.image(img_bw, use_column_width=True)
+	with col2:
+	    st.header("ECCV 16")
+	    st.image(out_img_eccv16, use_column_width=True)
+	with col3:
+	    st.header("SIGGRAPH 17")
+	    st.image(out_img_siggraph17, use_column_width=True)
 
 
 
