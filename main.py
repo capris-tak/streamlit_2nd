67d4052c4c1bcbc8ad5f225a2c5f1ab722b71635
capris-tak <109158237+capris-tak@users.noreply.github.com>
2022-07-23 05:35:26 +0900

    Update main.py

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ if uploaded_image is not None:
 	from colorizers import *
 	
 	parser = argparse.ArgumentParser()
-	parser.add_argument('-i','--img_path', type=str, default = str(binary_img))
+	parser.add_argument('-i','--img_path', type=str, default = str(_img))
 	parser.add_argument('--use_gpu', action='store_true', help='whether to use GPU')
 	parser.add_argument('-o','--save_prefix', type=str, default='saved', help='will save into this file with {eccv16.png, siggraph17.png} suffixes')
 	opt = parser.parse_args()
