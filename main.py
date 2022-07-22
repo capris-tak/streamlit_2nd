f01cecfd0ad3204d342224d086c58e08316f96fe
capris-tak <109158237+capris-tak@users.noreply.github.com>
2022-07-23 02:47:35 +0900

    Update main.py

--- main.py
+++ main.py
@@ -26,16 +26,16 @@ if uploaded_image is not None:
 	# load colorizers
 	colorizer_eccv16 = eccv16(pretrained=True).eval()
 	colorizer_siggraph17 = siggraph17(pretrained=True).eval()
-	if(opt.use_gpu):
-		colorizer_eccv16.cuda()
-		colorizer_siggraph17.cuda()
+	#if(opt.use_gpu):
+	#	colorizer_eccv16.cuda()
+	#	colorizer_siggraph17.cuda()
 
 	# default size to process images is 256x256
 	# grab L channel in both original ("orig") and resized ("rs") resolutions
 	#img = load_img(opt.img_path)
 	(tens_l_orig, tens_l_rs) = preprocess_img(image, HW=(256,256))
-	if(opt.use_gpu):
-		tens_l_rs = tens_l_rs.cuda()
+	#if(opt.use_gpu):
+	#	tens_l_rs = tens_l_rs.cuda()
 
 	# colorizer outputs 256x256 ab map
 	# resize and concatenate to original L channel
