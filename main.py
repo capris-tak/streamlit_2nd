ad28a7495673b17c05712727a27dd24e42948478
capris-tak <109158237+capris-tak@users.noreply.github.com>
2022-07-23 11:06:03 +0900

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,6 @@ uploaded_image = st.file_uploader('Choose an image..',type=['png', 'jpg','jpeg']
 
 if uploaded_image is not None:
         with tempfile.TemporaryDirectory() as tmpdir:
-	
 		st.write(tmpdir.name)
 		with open(os.path.join(tmpdir, uploaded_image.name), 'w+b') as fp:
 			st.write(fp)
