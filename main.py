0e4cec9362bcb7bf17f3abf8fb8b2648a61cf48b
capris-tak <109158237+capris-tak@users.noreply.github.com>
2022-07-23 11:43:11 +0900

    Update main.py

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ if uploaded_image is not None:
 		st.write(type(tmpdir))
 		st.write(Path(uploaded_image.name))
 		st.write(str(Path(uploaded_image.name)))
-		st.write(os.path.join(tmpdir, str(Path(uploaded_image.name))))
+		st.write(tmpdir+'/'+ str(Path(uploaded_image.name)))
 		with open(os.path.join(tmpdir, str(Path(uploaded_image.name))), 'wb') as fp:
 			fp.write_bytes(uploaded_image.getvalue())
 		#st.markdown("## Original PDF file")
