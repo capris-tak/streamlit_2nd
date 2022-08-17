036e632b5767657037a5736a2c4508f89047dffd
capris-tak <109158237+capris-tak@users.noreply.github.com>
2022-08-17 21:06:43 +0900

    Update main.py

--- main.py
+++ main.py
@@ -101,6 +101,7 @@ if uploaded_image is not None:
 
 
 st.subheader('EXAMPLES')
+st.write('each explanation is from ClipCap')
 
 st.write('A man with a beard and glasses standing in front of a table. Albert Einstein')
 col1, col2, col3 = st.columns(3)
@@ -140,7 +141,6 @@ with col3:
 	st.image("imgs/d3.jpg", use_column_width=True)
 
 
-st.write('each explanation is from ClipCap')
 st.write('ECCV : The European Conference on Computer Vision is a biennial research conference with the proceedings published by Springer Science+Business Media. Similar to ICCV in scope and quality, it is held those years which ICCV is not. It is considered to be one of the top conferences in computer vision, alongside CVPR and ICCV, with an A rating from the Australian Ranking of ICT Conferences and an A1 rating from the Brazilian ministry of education. The acceptance rate for ECCV 2010 was 24.4% for posters and 3.3% for oral presentations.')
 st.write('SIGGRAPH2017 : SIGGRAPH is the world’s largest, most influential annual conference and exhibition in computer graphics and interactive techniques: Five days of research results, demos, educational sessions, art, screenings, and hands-on interactivity featuring the community’s latest technical achievements, and three days of commercial exhibits displaying the industrys current hardware, software, and services.')
 #st.image(image, caption='Original',use_column_width=True)
