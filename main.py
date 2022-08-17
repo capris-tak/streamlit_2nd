c8f6302aac3fe191e1d1374da8628b5c01059112
capris-tak <109158237+capris-tak@users.noreply.github.com>
2022-08-18 00:49:00 +0900

    Update main.py

--- main.py
+++ main.py
@@ -103,7 +103,7 @@ if uploaded_image is not None:
 st.subheader('EXAMPLES')
 st.markdown('each explanation from ClipCap(CLIP model + GPT2 tokenizer) and Japanese from googletrans.')
 
-st.write('A man with a beard and glasses standing in front of a table.')
+st.caption('A man with a beard and glasses standing in front of a table.')
 st.write('テーブルの前に立っているひげと眼鏡を持った男。')
 col1, col2, col3 = st.columns(3)
 with col1:
